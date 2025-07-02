cbe3f6cebcd0dee2ee33080edd95e5b71abf6541
Ubvw <78339072+Ubvw@users.noreply.github.com>
2025-07-03 00:32:02 +0800

    Changed icon

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import streamlit as st
 
 pages = {
     "Landing": [
-        st.Page("landing_page.py", title="CredSight", icon="🏠"),
+        st.Page("landing_page.py", title="CredSight", icon="🛡️"),
     ],
     "Your account": [
         st.Page("dashboard.py", title="Fraud Detection Dashboard", icon="🛡️"),
